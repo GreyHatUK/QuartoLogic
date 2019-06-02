4f222156f793a12d6333310b78518a9bb6d0f0c5
Graham Fraser <fomhoire69@gmail.com>
2019-06-02 22:42:00 +0100

    a

--- QuartoLogic/QuartoBoard.cs
+++ QuartoLogic/QuartoBoard.cs
@@ -7,7 +7,7 @@ namespace QuartoLogic
     public class QuartoBoard
     {
         private const int _maxX = 4;
-        private const int _maxY = 4;
+        private const int _maxY = 4;        
         private QuartoPiece[,] _board;
         private List<QuartoPiece> _piecesToPlay;
         private QuartoPiece _selectedPeice;
@@ -72,8 +72,17 @@ namespace QuartoLogic
 
         public bool CheckIfWon()
         {
+            //Check Horizontal
+            for(int i= 0; i < 4; i++)
+            {
 
+            }
+            //Check Vertical
+            for (int i = 0; i < 4; i++)
+            {
 
+            }
+            //Check diagonal
         }
     }
 }
